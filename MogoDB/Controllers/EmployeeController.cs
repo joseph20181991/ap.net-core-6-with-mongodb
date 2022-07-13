3422943ba183e5db1d83fe8b1066c58fd38afc73
joseph rozario <rozariotokee@gmail.com>
2022-07-13 17:36:42 +0600

    first commit

---
+++ MogoDB/Controllers/EmployeeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MogoDB.Models;
+using MogoDB.Services;
+
+namespace MogoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService studentService;
+
+        public EmployeeController(IEmployeeService studentService)
+        {
+            this.studentService = studentService;
+        }
+        // GET: api/<StudentsController>
+        [HttpGet]
+        public ActionResult<List<Employee>> Get()
+        {
+            return studentService.Get();
+        }
+
+        // GET api/<StudentsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(string id)
+        {
+            var student = studentService.Get(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+
+            return student;
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public ActionResult<Employee> Post([FromBody] Employee student)
+        {
+            studentService.Create(student);
+
+            return CreatedAtAction(nameof(Get), new { id = student.Id }, student);
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Employee student)
+        {
+            var existingStudent = studentService.Get(id);
+
+            if (existingStudent == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+
+            studentService.Update(id, student);
+
+            return NoContent();
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var student = studentService.Get(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+
+            studentService.Remove(student.Id);
+
+            return Ok($"Student with Id = {id} deleted");
+        }
+    }
+}
---
+++ MogoDB/Services/IEmployeeService.cs
@@ -0,0 +1,50 @@
+﻿using MogoDB.Models;
+using MongoDB.Driver;
+
+namespace MogoDB.Services
+{
+    public interface IEmployeeService
+    {
+        List<Employee> Get();
+        Employee Get(string id);
+        Employee Create(Employee student);
+        void Update(string id, Employee student);
+        void Remove(string id);
+    }
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IMongoCollection<Employee> _students;
+
+        public EmployeeService(IEmployeeDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _students = database.GetCollection<Employee>(settings.EmployeesCollectionName);
+        }
+
+        public Employee Create(Employee student)
+        {
+            _students.InsertOne(student);
+            return student;
+        }
+
+        public List<Employee> Get()
+        {
+            return _students.Find(student => true).ToList();
+        }
+
+        public Employee Get(string id)
+        {
+            return _students.Find(student => student.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _students.DeleteOne(student => student.Id == id);
+        }
+
+        public void Update(string id, Employee student)
+        {
+            _students.ReplaceOne(student => student.Id == id, student);
+        }
+    }
+}
